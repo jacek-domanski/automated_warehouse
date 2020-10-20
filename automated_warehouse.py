731115dc8f9c834c76280c95a038ce18e6cd6910
jacek-domanski <j.domanski92@gmail.com>
2020-10-20 23:12:05 +0200

    :tada: Initialize project

---
+++ automated_warehouse.py
@@ -0,0 +1,11 @@
+class AutomatedWarehouse:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == '__main__':
+    automated_warehouse = AutomatedWarehouse()
+    automated_warehouse.run()
