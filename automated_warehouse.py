096b771069c089b563f68680565bc3d201833a66
jacek-domanski <j.domanski92@gmail.com>
2020-10-22 21:09:42 +0200

    :sparkles: ShortestPath class completed - method find returns cheapest path from a to b in as a list of Points

--- automated_warehouse.py
+++ automated_warehouse.py
@@ -8,10 +8,19 @@ class AutomatedWarehouse:
         self.warehouse = Warehouse(file)
 
     def run(self):
+        # shortest_path = ShortestPath(self.warehouse.matrix)
+        # a = choice(self.warehouse.starts)
+        # b = choice(self.warehouse.racks)
+        # shortest_path.find(a, b)
+
+        a = self.warehouse.matrix[1][0].coords
+        b = self.warehouse.matrix[4][2].coords
         shortest_path = ShortestPath(self.warehouse.matrix)
-        a = choice(self.warehouse.starts)
-        b = choice(self.warehouse.racks)
-        shortest_path.find(a, b)
+        path = shortest_path.find(a, b)
+
+        print(f'Shortest path from {a} to {b}:')
+        for point in path:
+            print(f'{point}')
 
 
 if __name__ == '__main__':
--- shortest_path.py
+++ shortest_path.py
@@ -2,34 +2,81 @@ from tiles import *
 
 
 class Node:
-    def __init__(self):
+    def __init__(self, tile):
+        self.tile = tile
         self.links = []
-        self.cost = float('inf')
+        self.reset()
 
-    def add_links(self, all_nodes, tile_neighbours):
-        for n in tile_neighbours:
-            x = n.coords.x
-            y = n.coords.y
+    def add_links(self, all_nodes):
+        for neighbour in self.tile.connections:
+            x = neighbour.coords.x
+            y = neighbour.coords.y
             self.links.append(all_nodes[y][x])
+
+    def reset(self):
+        self.cost = float('inf')
+        self.best_from = None
 
 
 class ShortestPath:
+    """Finds cheapest path from a to b"""
     def __init__(self, matrix):
         self.nodes = []
 
-        self.add_nodes(matrix)
+        self.__add_nodes(matrix)
+        self.__add_links_to_nodes()
 
-        for y, row in enumerate(self.nodes):
-            for x, node in enumerate(row):
-                node.add_links(self.nodes, matrix[y][x].connections)
+        self.a = None
+        self.b = None
+        self.cost = 0
 
-    def add_nodes(self, matrix):
+    def __add_nodes(self, matrix):
         for row_of_tiles in matrix:
             row_of_nodes = []
-            for _ in row_of_tiles:
-                row_of_nodes.append(Node())
+            for tile in row_of_tiles:
+                row_of_nodes.append(Node(tile))
             self.nodes.append(row_of_nodes)
 
-    def find(self, a, b):
-        pass
-        # Nothing was tested yet
+    def __add_links_to_nodes(self):
+        for row in self.nodes:
+            for node in row:
+                node.add_links(self.nodes)
+
+    def find(self, a: Point, b: Point):
+        """Main method that returns cheapest way as a list of Points"""
+        self.__setup(a, b)
+        self.__start_dijkstra()
+        return self.__build_path()
+
+    def __setup(self, a, b):
+        self.__reset_nodes()
+        self.a = a
+        self.b = b
+
+    def __reset_nodes(self):
+        for row in self.nodes:
+            for node in row:
+                node.reset()
+
+    def __start_dijkstra(self):
+        self.starting_node = self.nodes[self.a.y][self.a.x]
+        self.starting_node.cost = 0
+        self.__check_node(self.starting_node)
+
+    def __check_node(self, node):
+        for link in node.links:
+            if node.cost+1 < link.cost:
+                link.best_from = node
+                link.cost = node.cost+1
+
+                self.__check_node(link)
+
+    def __build_path(self):
+        path = []
+        node = self.nodes[self.b.y][self.b.x]
+
+        while node != self.starting_node:
+            path.insert(0, node.tile.coords)
+            node = node.best_from
+
+        return path
